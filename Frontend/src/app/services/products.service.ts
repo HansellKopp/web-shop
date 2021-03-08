5d307039eae76f21d34bb8124473f936d013dc41
Hansell Kopp <hansell.kopp@frontend-gmbh.de>
2021-03-08 09:35:49 +0100

    add static server

--- Frontend/src/app/services/products.service.ts
+++ Frontend/src/app/services/products.service.ts
@@ -1,45 +1,47 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment'
 import { Product } from '../models/products.interface';
 import { ProductComponent } from '../pages/product/product.component';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: `root`
 })
 export class ProductsService {
-
+  
+  rootPath = environment.rootPath
   constructor(
     private http: HttpClient
   ) { }
 
   getCategories() {
-    return this.http.get<string[]>('http://localhost:3000/categories')
+    return this.http.get<string[]>(`${this.rootPath}/categories`)
   }
 
   getProducts(pageIndex?: number) {
-    return this.http.get<Product[]>('http://localhost:3000/items')
+    return this.http.get<Product[]>(`${this.rootPath}/items`)
   }
 
   getProductById(id: string) {
-    return this.http.get<Product>('http://localhost:3000/items/' + id)
+    return this.http.get<Product>(`${this.rootPath}/items/` + id)
   }
 
   getProductsQuery(searchTerm: string) {
-    return this.http.get<Product[]>('http://localhost:3000/items/query/' + searchTerm)
+    return this.http.get<Product[]>(`${this.rootPath}/items/query/` + searchTerm)
   }
 
   postProduct(data: Product) {
-    return this.http.post('http://localhost:3000/items', data)
+    return this.http.post(`${this.rootPath}/items`, data)
   }
 
   putProduct(id:string, data: Product) {
-    return this.http.put(`http://localhost:3000/items/${id}`, data)
+    return this.http.put(`${this.rootPath}/items/${id}`, data)
   }
   deleteProduct(id: string) {
-    return this.http.delete('http://localhost:3000/items/' + id)
+    return this.http.delete(`${this.rootPath}/items/` + id)
   }
   postImages(data: FormData) {
-    return this.http.post('http://localhost:3000/images/upload', data)
+    return this.http.post(`${this.rootPath}/images/upload`, data)
   }
 
 }
--- Frontend/src/app/services/users.service.ts
+++ Frontend/src/app/services/users.service.ts
@@ -2,21 +2,22 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../models/users.interface';
 import { AuthService } from '@auth0/auth0-angular';
+import { environment } from 'src/environments/environment'
 import { Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
-
+  rootPath = environment.rootPath
   constructor(private http: HttpClient, private auth: AuthService) {}
   
   getUsers() {
-    return this.http.get<User[]>('http://localhost:3000/users')
+    return this.http.get<User[]>(`${this.rootPath}/users`)
   }
 
   postUser(data: User) {
-    return this.http.post<User[]>('http://localhost:3000/users', data)
+    return this.http.post<User[]>(`${this.rootPath}/users`, data)
   }
 
   getCurrentUser(): Observable<any> {
